00fdf63cfc6ed05837a0b85efb4afd8f678217f1
rayaahmada <r.idrisa@gmail.com>
2021-01-27 12:08:31 +0300

    Add project files.

---
+++ dataAccess1/registerProduct.Designer.cs
@@ -0,0 +1,243 @@
+﻿
+namespace dataAccess1
+{
+    partial class registerProduct
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblProductID = new System.Windows.Forms.Label();
+            this.txtProductID = new System.Windows.Forms.TextBox();
+            this.txtItemName = new System.Windows.Forms.TextBox();
+            this.lblItemName = new System.Windows.Forms.Label();
+            this.txtDesign = new System.Windows.Forms.TextBox();
+            this.lblDesign = new System.Windows.Forms.Label();
+            this.lblColor = new System.Windows.Forms.Label();
+            this.cmbColor = new System.Windows.Forms.ComboBox();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dtpUpdateDate = new System.Windows.Forms.DateTimePicker();
+            this.btDelete = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(162, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(143, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CRUD PART 1";
+            // 
+            // lblProductID
+            // 
+            this.lblProductID.AutoSize = true;
+            this.lblProductID.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblProductID.Location = new System.Drawing.Point(86, 45);
+            this.lblProductID.Name = "lblProductID";
+            this.lblProductID.Size = new System.Drawing.Size(95, 20);
+            this.lblProductID.TabIndex = 1;
+            this.lblProductID.Text = "Product ID";
+            this.lblProductID.Click += new System.EventHandler(this.lblProductID_Click);
+            // 
+            // txtProductID
+            // 
+            this.txtProductID.Location = new System.Drawing.Point(216, 45);
+            this.txtProductID.Name = "txtProductID";
+            this.txtProductID.Size = new System.Drawing.Size(218, 20);
+            this.txtProductID.TabIndex = 2;
+            // 
+            // txtItemName
+            // 
+            this.txtItemName.Location = new System.Drawing.Point(215, 82);
+            this.txtItemName.Name = "txtItemName";
+            this.txtItemName.Size = new System.Drawing.Size(218, 20);
+            this.txtItemName.TabIndex = 4;
+            // 
+            // lblItemName
+            // 
+            this.lblItemName.AutoSize = true;
+            this.lblItemName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblItemName.Location = new System.Drawing.Point(85, 82);
+            this.lblItemName.Name = "lblItemName";
+            this.lblItemName.Size = new System.Drawing.Size(96, 20);
+            this.lblItemName.TabIndex = 3;
+            this.lblItemName.Text = "Item Name";
+            // 
+            // txtDesign
+            // 
+            this.txtDesign.Location = new System.Drawing.Point(216, 117);
+            this.txtDesign.Name = "txtDesign";
+            this.txtDesign.Size = new System.Drawing.Size(218, 20);
+            this.txtDesign.TabIndex = 6;
+            // 
+            // lblDesign
+            // 
+            this.lblDesign.AutoSize = true;
+            this.lblDesign.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDesign.Location = new System.Drawing.Point(86, 117);
+            this.lblDesign.Name = "lblDesign";
+            this.lblDesign.Size = new System.Drawing.Size(65, 20);
+            this.lblDesign.TabIndex = 5;
+            this.lblDesign.Text = "Design";
+            // 
+            // lblColor
+            // 
+            this.lblColor.AutoSize = true;
+            this.lblColor.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblColor.Location = new System.Drawing.Point(86, 156);
+            this.lblColor.Name = "lblColor";
+            this.lblColor.Size = new System.Drawing.Size(51, 20);
+            this.lblColor.TabIndex = 7;
+            this.lblColor.Text = "Color";
+            // 
+            // cmbColor
+            // 
+            this.cmbColor.FormattingEnabled = true;
+            this.cmbColor.Items.AddRange(new object[] {
+            "Red",
+            "Green",
+            "Yellow",
+            "White",
+            "Black"});
+            this.cmbColor.Location = new System.Drawing.Point(216, 155);
+            this.cmbColor.Name = "cmbColor";
+            this.cmbColor.Size = new System.Drawing.Size(218, 21);
+            this.cmbColor.TabIndex = 8;
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInsert.Location = new System.Drawing.Point(27, 231);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(110, 34);
+            this.btnInsert.TabIndex = 9;
+            this.btnInsert.Text = "Insert";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(27, 271);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(480, 169);
+            this.dataGridView1.TabIndex = 10;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(216, 231);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 34);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Update";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(88, 188);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 20);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Date";
+            // 
+            // dtpUpdateDate
+            // 
+            this.dtpUpdateDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtpUpdateDate.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpUpdateDate.Location = new System.Drawing.Point(215, 188);
+            this.dtpUpdateDate.Name = "dtpUpdateDate";
+            this.dtpUpdateDate.Size = new System.Drawing.Size(218, 22);
+            this.dtpUpdateDate.TabIndex = 13;
+            // 
+            // btDelete
+            // 
+            this.btDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btDelete.Location = new System.Drawing.Point(397, 231);
+            this.btDelete.Name = "btDelete";
+            this.btDelete.Size = new System.Drawing.Size(110, 34);
+            this.btDelete.TabIndex = 14;
+            this.btDelete.Text = "Delete";
+            this.btDelete.UseVisualStyleBackColor = true;
+            this.btDelete.Click += new System.EventHandler(this.btDelete_Click);
+            // 
+            // registerProduct
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(531, 452);
+            this.Controls.Add(this.btDelete);
+            this.Controls.Add(this.dtpUpdateDate);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnInsert);
+            this.Controls.Add(this.cmbColor);
+            this.Controls.Add(this.lblColor);
+            this.Controls.Add(this.txtDesign);
+            this.Controls.Add(this.lblDesign);
+            this.Controls.Add(this.txtItemName);
+            this.Controls.Add(this.lblItemName);
+            this.Controls.Add(this.txtProductID);
+            this.Controls.Add(this.lblProductID);
+            this.Controls.Add(this.label1);
+            this.Name = "registerProduct";
+            this.Text = "registerProduct";
+            this.Load += new System.EventHandler(this.registerProduct_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblProductID;
+        private System.Windows.Forms.TextBox txtProductID;
+        private System.Windows.Forms.TextBox txtItemName;
+        private System.Windows.Forms.Label lblItemName;
+        private System.Windows.Forms.TextBox txtDesign;
+        private System.Windows.Forms.Label lblDesign;
+        private System.Windows.Forms.Label lblColor;
+        private System.Windows.Forms.ComboBox cmbColor;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dtpUpdateDate;
+        private System.Windows.Forms.Button btDelete;
+    }
+}
---
+++ dataAccess1/registerProduct.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dataAccess1
+{
+    public partial class registerProduct : Form
+    {
+        public registerProduct()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection conn = new SqlConnection("Data Source=DESKTOP-CBER67G;" +
+                "Initial Catalog=CDemo;Integrated Security=True");
+        private void lblProductID_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            
+            conn.Open();
+            SqlCommand command = new SqlCommand("insert into tbl_Product" +
+                "(ProductID,ItemName,Design,Color,InsertDate) values('" + int.Parse(txtProductID.Text) + "'," +
+                "'" + txtItemName.Text+ "','"+txtDesign.Text+"','"+cmbColor.Text+"',getdate())",conn);
+            command.ExecuteNonQuery();
+           // MessageBox.Show("Successfully Inserted");
+            conn.Close();
+            BindData();
+        }
+
+        void BindData()
+        {
+            SqlCommand command = new SqlCommand("Select * from tbl_product", conn);
+            SqlDataAdapter sd = new SqlDataAdapter(command);
+            DataTable dt = new DataTable();
+            sd.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+            SqlCommand command = new SqlCommand("update tbl_Product " +
+                "set ItemName='" + txtItemName.Text + "',Design='" + txtDesign.Text + "'" +
+                ",Color='" + cmbColor.Text + "',UpdateDate = '"+DateTime.Parse(dtpUpdateDate.Text)+"' where productID='" + int.Parse(txtProductID.Text) + "'", conn);
+            command.ExecuteNonQuery();
+            conn.Close();
+            MessageBox.Show("Successsfully Updated");
+            BindData();
+
+
+        }
+
+        private void registerProduct_Load(object sender, EventArgs e)
+        {
+            BindData();
+        }
+
+        private void btDelete_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+            SqlCommand command = new SqlCommand("delete from tbl_Product where " +
+                "ProductID = '"+int.Parse(txtProductID.Text)+"'", conn);
+            command.ExecuteNonQuery();
+            conn.Close();
+            MessageBox.Show("Successfully Deleted!");
+            BindData();
+        }
+    }
+}
